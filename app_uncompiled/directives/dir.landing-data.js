d496f5bb5f04b0defc38af6373898327963eeac3
Julius Alvarado <javascript.uiux@gmail.com>
2019-10-21 11:21:43 -0700

    deleted old ref code

---
+++ app_uncompiled/directives/dir.landing-data.js
@@ -0,0 +1,13 @@
+(function () {
+    'use strict';
+
+    angular.module('rsCloudApp').directive('landingData', [
+        LandingDataDirectiveClass
+    ]);
+
+    function LandingDataDirectiveClass() {
+        return {
+            templateUrl: 'directives/temps/temp.landing-data.html'
+        }
+    }
+}());
