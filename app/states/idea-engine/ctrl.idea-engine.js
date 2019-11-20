0052533cd821f41e956448111c9d820a56500120
Julius Alvarado <javascript.uiux@gmail.com>
2019-11-19 19:11:22 -0800

    i've decided that just pasting the link directly in the iframe
    is probably the most secure, getting to the 'idea-engine' route is what ought to be defended

--- app/states/idea-engine/ctrl.idea-engine.js
+++ app/states/idea-engine/ctrl.idea-engine.js
@@ -2,14 +2,13 @@
     'use strict';
 
     angular.module('rsCloudApp').controller('IdeaEngineCtrl', [
-        '$location', 'smoothScroll', '$rootScope', '$scope', 'rsAuth',
+        '$location', 'smoothScroll', '$rootScope', '$scope', 'rsAuth', '$sce',
         IdeaEngineCtrlClass
     ]);
 
-    function IdeaEngineCtrlClass() {
+    function IdeaEngineCtrlClass($location, smoothScroll, $rootScope, $scope, rsAuth, $sce) {
 
         const vm = this;
-        vm.vidOne = "https://firebasestorage.googleapis.com/v0/b/redstone-auto.appspot.com/o/idea-engine_add-field.mp4?alt=media&token=e4923aba-d817-48cc-a6d4-b655e3d9b897";
 
     }
 
